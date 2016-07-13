5e0d551cc2b74725d6bf7f761698f34f12b833f9
ran.wei@york.ac.uk <ran.wei@york.ac.uk>
2016-07-13 11:37:12 +0100

    made symbolic regression problem an individual project

--- probabilistic.smoothing.ecj.stochastic/src/probabilistic/smoothing/ecj/functionset/AbstractAttributeNode.java
+++ probabilistic.smoothing.ecj.stochastic/src/probabilistic/smoothing/ecj/functionset/AbstractAttributeNode.java
@@ -5,6 +5,7 @@ import ec.gp.GPNode;
 public abstract class AbstractAttributeNode extends GPNode{
 
 	public double stochastic_cost = 0.0;
+	
 	@Override
 	public int expectedChildren() {
 		return 1;
---
+++ symbolic.regression.stochastic/src/symbolic/regression/stochastic/terminals/Stochastic_Terminal.java
@@ -0,0 +1,7 @@
+package symbolic.regression.stochastic.terminals;
+
+import ec.gp.GPNode;
+
+public abstract class Stochastic_Terminal extends GPNode{
+
+}
